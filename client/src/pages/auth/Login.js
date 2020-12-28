5367142dabe3d6e5df8388a05decaaebe83187b6
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-28 16:48:33 -0500

    added google login

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import { useDispatch } from 'react-redux'
-import { auth } from "../../firebase"
+import { auth, googleAuthProvider } from "../../firebase"
 import { toast } from "react-toastify"
 import { Button } from 'antd'
-import { MailOutlined } from '@ant-design/icons';
+import { MailOutlined, GoogleOutlined } from '@ant-design/icons';
 
 const Login = ({ history }) => {
     const [ email, setEmail ] = useState("mooredoug6@gmail.com")
@@ -36,6 +36,25 @@ const Login = ({ history }) => {
             toast.error(error.message)
             setLoading(false)
         }
+    }
+
+    const googleLogin = async () => {
+        auth.signInWithPopup(googleAuthProvider).then(async (result) => {
+            const { user } = result
+            const idTokenResult = await user.getIdTokenResult()
+            dispatch({
+                type: "LOGGED_IN_USER",
+                payload: {
+                    user: user.email,
+                    token: idTokenResult.token
+                }
+            })
+            history.push('/')
+        })
+        .catch(err => {
+            console.error(err)
+            toast.error(err.message)
+        })
     }
 
     const loginForm = () => (
@@ -79,8 +98,24 @@ const Login = ({ history }) => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4>Login</h4>
+                    {loading ? (
+                        <h4 className="text-danger">Loading...</h4>
+                    ) : (
+                        <h4>Login</h4> 
+                    )}
                     {loginForm()}
+
+                    <Button 
+                        onClick={googleLogin}
+                        type="danger"
+                        className="mb-3"
+                        block
+                        shape="round"
+                        icon={ <GoogleOutlined />}
+                        size="large"
+                    >
+                        Login with Google
+                    </Button>
                 </div>           
             </div>
         </div>
