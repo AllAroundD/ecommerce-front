68823945bc1f2b377109b32ea86d44881b9b6528
AllAroundD <AllAroundDWeb@gmail.com>
2021-01-24 17:22:20 -0500

    added firebase auth check

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -5,6 +5,16 @@ import { toast } from "react-toastify"
 import { Button } from 'antd'
 import { MailOutlined, GoogleOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+const createOrUpdateUser = async (authtoken) => {
+    return await axios.post(
+        `${process.env.REACT_APP_API}/create-or-update-user`, {}, {
+        headers: {
+            authtoken
+        }
+    })
+}
 
 const Login = ({ history }) => {
     const [ email, setEmail ] = useState("mooredoug6@gmail.com")
@@ -29,14 +39,18 @@ const Login = ({ history }) => {
             const {user} = result
             const idTokenResult = await user.getIdTokenResult()
 
-            dispatch({
-                type: "LOGGED_IN_USER",
-                payload: {
-                    email: user.email,
-                    token: idTokenResult.token
-                }
-            })
-            history.push('/')
+            createOrUpdateUser(idTokenResult.token)
+            .then((res) => console.log("CREATE OR UPDATE RES", res))
+            .catch()
+
+            // dispatch({
+            //     type: "LOGGED_IN_USER",
+            //     payload: {
+            //         email: user.email,
+            //         token: idTokenResult.token
+            //     }
+            // })
+            // history.push('/')
 
         } catch (error) {
             console.error(error)
---
+++ firebase/index.js
@@ -0,0 +1,11 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-4e66d.firebaseio.com"
+});
+
+module.exports = admin
