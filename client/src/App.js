2e8e186c1b197fa3105e81f477cde50461851c1a
AllAroundD <AllAroundDWeb@gmail.com>
2021-01-28 22:14:28 -0500

    added protected user route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Home from './pages/Home'
 import Header from './components/nav/Header'
 import RegisterComplete from './pages/auth/RegisterComplete'
 import ForgotPassword from './pages/auth/ForgotPassword'
+import History from './pages/user/History'
+import UserRoute from './components/routes/UserRoute'
 
 import { auth } from './firebase'
 import {useDispatch} from 'react-redux'
@@ -48,11 +50,12 @@ useEffect(() => {
     <Header />
     <ToastContainer />
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Register}/>
-      <Route exact path="/register/complete" component={RegisterComplete}/>
-      <Route exact path="/forgot/password" component={ForgotPassword}/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/register/complete" component={RegisterComplete} />
+      <Route exact path="/forgot/password" component={ForgotPassword} />
+      <UserRoute exact path="/user/history" component={History} />
     </Switch>
     </>
   )
