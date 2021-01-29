48827a2fd8fd7855ddd16b4ead123c273cdab0eb
AllAroundD <AllAroundDWeb@gmail.com>
2021-01-29 17:08:58 -0500

    category CRUD

--- controllers/category.js
+++ controllers/category.js
@@ -1,37 +1,61 @@
 const Category = require('../models/category')
 const slugify = require('slugify')
 
-exports.create = async (req, res) =>{
-    try {
-        const {name} = req.body
-        // const category = await new Category({ name, slug: slugify(name) }).save()
-        res.json(await new Category({ name, slug: slugify(name) }).save())
-    } catch (error) {
-        console.error(error)
-        res.status(400).send('Create category failed')
-    }
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body
+    // const category = await new Category({ name, slug: slugify(name) }).save()
+    res.json(await new Category({ name, slug: slugify(name) }).save())
+  } catch (error) {
+    console.error(error)
+    res.status(400).send('Create category failed')
+  }
 }
 
-exports.list = async (req, res) =>{
-    try {
-        
-    } catch (error) {
-        
-    }
+exports.list = async (req, res) => {
+  try {
+    res.json(await Category.find({}).sort({ createdAt: -1 }).exec())
+    console.log(`Categories retrieved.`)
+  } catch (error) {
+    console.error(error)
+    res.status(400).send('Failed to retrieve categories')
+  }
 }
 
-exports.read = async (req, res) =>{
-    try {
-        
-    } catch (error) {
-        
-    }
+exports.read = async (req, res) => {
+  try {
+    let category = await Category.findOne({ slug: req.params.slug }).exec()
+    console.log(`Category retrieved: ${category}`)
+    res.json(category)
+  } catch (error) {
+    console.error(error)
+    res.status(400).send('Failed to retrieve category')
+  }
 }
 
-exports.update = async (req, res) =>{
-    
+exports.update = async (req, res) => {
+  try {
+    const { name } = req.body
+    const updated = await Category.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true },
+    )
+    console.log(`Category updated: ${updated}`)
+    res.json(updated)
+  } catch (error) {
+    console.error(error)
+    res.status(400).send('Failed to update category')
+  }
 }
 
-exports.remove = async (req, res) =>{
-    
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Category.findOneAndDelete({ slug: req.params.slug })
+    console.log(`Category deleted: ${deleted}`)
+    res.json(deleted)
+  } catch (error) {
+    console.error(error)
+    res.status(400).send('Failed to delete category')
+  }
 }
