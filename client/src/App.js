26baa3e35816df676a32cc0d8c920dda217e8058
AllAroundD <AllAroundDWeb@gmail.com>
2021-02-15 11:04:50 -0500

    moved all products page and added default image

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import CategoryUpdate from './pages/admin/category/CategoryUpdate'
 import SubCreate from './pages/admin/sub/SubCreate'
 import SubUpdate from './pages/admin/sub/SubUpdate'
 import ProductCreate from './pages/admin/product/ProductCreate'
+import AllProducts from './pages/admin/product/AllProducts'
 
 import { auth } from './firebase'
 import { useDispatch } from 'react-redux'
@@ -77,6 +78,7 @@ const App = () => {
         <AdminRoute exact path="/admin/sub" component={SubCreate} />
         <AdminRoute exact path="/admin/sub/:slug" component={SubUpdate} />
         <AdminRoute exact path="/admin/product" component={ProductCreate} />
+        <AdminRoute exact path="/admin/products" component={AllProducts} />
       </Switch>
     </>
   )
--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card } from 'antd'
+import laptop from '../../images/laptop.png'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
 const { Meta } = Card
 
@@ -7,11 +9,24 @@ const AdminProductCard = ({ product }) => {
   const { title, description, images } = product
   return (
     <Card
-      cover={<img src={images && images.length ? images[0].url : ''} />}
-      style={{ height: '150px', objectFit: 'cover' }}
-      className="m-2"
+      hoverable
+      cover={
+        <img
+          src={images && images.length ? images[0].url : laptop}
+          style={{ height: '150px', objectFit: 'cover' }}
+          className="p-1"
+          alt="product"
+        />
+      }
+      actions={[
+        <EditOutlined className="text-warning" />,
+        <DeleteOutlined className="text-danger" />,
+      ]}
     >
-      <Meta title={title} description={description} />
+      <Meta
+        title={title}
+        description={`${description && description.substring(0, 50)}...`}
+      />
     </Card>
   )
 }
