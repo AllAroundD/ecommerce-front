610282aa493b8d6af8e14825bfaa27390009359e
AllAroundD <AllAroundDWeb@gmail.com>
2021-02-22 22:16:03 -0500

    added StarRating model and initial rating to SingleProduct

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Card } from 'antd'
+import React, { useState } from 'react'
+import { Card, Tabs } from 'antd'
 import { Link } from 'react-router-dom'
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Carousel } from 'react-responsive-carousel'
 import Laptop from '../../images/laptop.png'
 import ProductListItems from './ProductListItems'
+import StarRating from 'react-star-ratings'
+
+const { TabPane } = Tabs
 
 const SingleProduct = ({ product }) => {
-  const { title, images } = product
+  const { title, images, description, _id } = product
+  const [rating, setRating] = useState(0)
 
   return (
     <>
@@ -27,10 +31,31 @@ const SingleProduct = ({ product }) => {
             }
           ></Card>
         )}
+
+        <Tabs type="card">
+          <TabPane tab="Description" key="1">
+            {description && description}
+          </TabPane>
+          <TabPane tab="More" key="2">
+            Call us on xxxx xxx xxx to learn more about this product.
+          </TabPane>
+        </Tabs>
       </div>
 
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
+        <StarRating
+          name={_id}
+          numberOfStars={5}
+          rating={rating}
+          changeRating={(newRating, name) => {
+            console.log('newRating', newRating, 'name', name)
+            setRating(newRating)
+          }}
+          isSelectable={true}
+          starRatedColor="red"
+        />
+
         <Card
           actions={[
             <>
