a59c3dc835e72044996e152f2b55fdea2e82698f
AllAroundD <AllAroundDWeb@gmail.com>
2021-02-28 12:09:47 -0500

    added averageRating to SingleProduct and HomePage

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -3,40 +3,48 @@ import { Card } from 'antd'
 import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons'
 import laptop from '../../images/laptop.png'
 import { Link } from 'react-router-dom'
+import { showAverage } from '../../functions/rating'
 
 const { Meta } = Card
 
 const ProductCard = ({ product }) => {
   const { title, description, images, slug } = product
   return (
-    <Card
-      cover={
-        <img
-          src={images && images.length ? images[0].url : laptop}
-          style={{ height: '150px', objectFit: 'cover' }}
-          className="p-1"
-          alt="product"
+    <>
+      {product && product.ratings && product.ratings.length > 0 ? (
+        showAverage(product)
+      ) : (
+        <div className="text-center pt-1 pb-3">No rating yet</div>
+      )}
+      <Card
+        cover={
+          <img
+            src={images && images.length ? images[0].url : laptop}
+            style={{ height: '150px', objectFit: 'cover' }}
+            className="p-1"
+            alt="product"
+          />
+        }
+        actions={[
+          <Link to={`/product/${slug}`}>
+            <EyeOutlined className="text-warning" />
+            <br />
+            View Product
+          </Link>,
+          <>
+            <ShoppingCartOutlined className="text-danger" />
+            <br />
+            Add to Cart
+          </>,
+        ]}
+      >
+        <Meta
+          title={title}
+          description={`${description && description.substring(0, 50)}...`}
         />
-      }
-      actions={[
-        <Link to={`/product/${slug}`}>
-          <EyeOutlined className="text-warning" />
-          <br />
-          View Product
-        </Link>,
-        <>
-          <ShoppingCartOutlined className="text-danger" />
-          <br />
-          Add to Cart
-        </>,
-      ]}
-    >
-      <Meta
-        title={title}
-        description={`${description && description.substring(0, 50)}...`}
-      />
-      {title}
-    </Card>
+        {title}
+      </Card>
+    </>
   )
 }
 
