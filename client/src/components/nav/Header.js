e5753d25a59440a9536b0c84f26f64584e364a3a
AllAroundD <AllAroundDWeb@gmail.com>
2021-03-15 21:37:58 -0400

    added search redux and component

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,71 +1,89 @@
 import React, { useState } from 'react'
 import { Menu } from 'antd'
-import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined, LogoutOutlined } from '@ant-design/icons';
+import {
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import firebase from 'firebase'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import Search from '../../components/forms/Search'
 
-const { SubMenu, Item } = Menu;
+const { SubMenu, Item } = Menu
 
 const Header = () => {
-    const [ current, setCurrent ] = useState('home')
-    let dispatch = useDispatch()
-    let {user} = useSelector((state) => ({ ...state }))
+  const [current, setCurrent] = useState('home')
+  let dispatch = useDispatch()
+  let { user } = useSelector((state) => ({ ...state }))
 
-    let history = useHistory()
+  let history = useHistory()
 
-    const handleClick = (e) => {
-        // console.log(e.key)
-        setCurrent(e.key)
-    }
+  const handleClick = (e) => {
+    // console.log(e.key)
+    setCurrent(e.key)
+  }
 
-    const logout = () => {
-        firebase.auth().signOut()
-        dispatch({
-            type: "LOGOUT",
-            payload: null,
-        })
-        history.push('/login')
-    }
+  const logout = () => {
+    firebase.auth().signOut()
+    dispatch({
+      type: 'LOGOUT',
+      payload: null,
+    })
+    history.push('/login')
+  }
 
-    return (
-
+  return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Item key="home" icon={<AppstoreOutlined />}>
-            <Link to="/">Home</Link>
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
+      </Item>
+
+      {!user && (
+        <Item key="register" icon={<UserAddOutlined />} className="float-right">
+          <Link to="/register">Register</Link>
         </Item>
+      )}
 
-        {!user && (
-            <Item key="register" icon={<UserAddOutlined />}  className="float-right">
-                <Link to="/register">Register</Link>
+      {!user && (
+        <Item key="login" icon={<UserOutlined />} className="float-right">
+          <Link to="/login">Login</Link>
+        </Item>
+      )}
+
+      {user && (
+        <SubMenu
+          key="User"
+          icon={<SettingOutlined />}
+          title={user.email && user.email.split('@')[0]}
+          className="float-right"
+        >
+          {user && user.role === 'subscriber' && (
+            <Item>
+              <Link to="/user/history">Dashboard</Link>
             </Item>
-        )}
+          )}
 
-        {!user && (
-            <Item key="login" icon={<UserOutlined />} className="float-right">
-                <Link to="/login">Login</Link>
+          {user && user.role === 'admin' && (
+            <Item>
+              <Link to="/admin/dashboard">Dashboard</Link>
             </Item>
-        )}
-        
-        {user && (
-            <SubMenu 
-                key="User" 
-                icon={<SettingOutlined />} 
-                title={user.email && user.email.split('@')[0]} 
-                className="float-right"
-            >
-                {user && user.role === 'subscriber' && <Item><Link to="/user/history">Dashboard</Link></Item>}
-                
-                {user && user.role === 'admin' && <Item><Link to="/admin/dashboard">Dashboard</Link></Item>}
+          )}
 
-                <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
-            </SubMenu>
-        )}
-      </Menu>
+          <Item icon={<LogoutOutlined />} onClick={logout}>
+            Logout
+          </Item>
+        </SubMenu>
+      )}
 
-    )
-
+      <span className="float-right p-1">
+        <Search />
+      </span>
+    </Menu>
+  )
 }
 
 export default Header
